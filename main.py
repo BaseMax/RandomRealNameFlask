511aef41e78a28b5fe7cde6acd87d06ba81a81f0
AliiAhmadi <aliahmadi.github@gmail.com>
2022-09-12 01:01:39 -0400

    Add some routes

--- main.py
+++ main.py
@@ -28,16 +28,36 @@ def get_several_user(count):
 
 @app.route("/get/<int:count>/<string:gender>")
 def get_user_specific_gender(count, gender):
-    if gender.lower() != Gender.male.name and gender.lower() != Gender.female.name:
+    types = [Gender.male.name, Gender.female.name, Gender.both.name]
+    if gender.lower() in types:
+        users = create_several_user(amount=count, gender=gender.lower())
+        return render_template(
+            "users.html", title=f"{count} user", users=users, status=1
+        )
+    else:
         return render_template(
             "error.html",
             title="ERROR",
             status=0,
             message="Oops, sorry. Something does not go as we expected.",
         )
+
+
+@app.route("/get/<string:gender>")
+def get_one_user_with_gender(gender):
+    types = [Gender.male.name, Gender.female.name, Gender.both.name]
+    if gender.lower() in types:
+        user = create_user(gender=gender.lower())
+        return render_template(
+            "user.html", title="1 user", user=convert_to_string(user), status=1
+        )
     else:
-        users = create_several_user(amount=count, gender=gender)
-    return render_template("users.html", title=f"{count} user", users=users, status=1)
+        return render_template(
+            "error.html",
+            title="ERROR",
+            status=0,
+            message="Oops, sorry. Something does not go as we expected.",
+        )
 
 
 @app.errorhandler(404)
